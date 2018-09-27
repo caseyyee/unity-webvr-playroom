b8c711ae7ef84f7892645d8bd6f942eea7995e82
Casey Yee <caseyyee.ca@gmail.com>
2018-09-27 12:31:21 -0700

    Updates scene, adds controls

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
